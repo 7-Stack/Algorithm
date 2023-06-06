fe5b393b5799eecf7ae5cc1288b1263909ccecbf
7-Stack <ashleyculture@yahoo.com>
2023-06-06 08:59:01 +0100

    added livinus task

---
+++ LivinusTask.js
@@ -0,0 +1,134 @@
+// My first individual task from Livinus
+const numbers = [2, 4, 6, 8];
+// Function to square each array element
+function squareArray(arr) {
+const SquaredArray = [];
+for (let i = 0; i<arr.length; i++) {
+const squaredValue = arr[i] * arr[i];
+
+SquaredArray.push(squaredValue);
+}
+return SquaredArray;
+}
+
+// Call the function and log the result
+const squaredNumbers = squareArray(numbers);
+console.log(squaredNumbers);
+
+// Practicing with the functions first
+class Statistics {
+constructor(arrayX, arrayY) {
+this.arrayX = arrayY;
+this.arrayX = arrayY;
+ }
+}
+sum(); {
+let result = [];
+for (let i = 0; i < this.arrayX.length; i++) {
+    result.push(this.array[i] + this.arrayY[i]);
+}
+return result;
+}
+
+product(); {
+let result = [];
+for (let i = 0; i < this.arrayX.length; 1++)  {
+result.push(this.array[i] * this.arrayY[i]);
+    }
+    return result;
+}
+// Creating a class called Statistics
+class Statistics {
+arrayX= [1, 2, 3, 4, 5]
+arrayY = [6, 7, 8, 9, 10]
+}
+
+// class to compute and return the product and sum of arrayX & arrayY using method
+const arrayMath = new ArrayMath(arrayX, arrayY)
+// Product of arrayX and arrayY
+console.log(arrayMath.product());
+
+// Summaton of arrayX and arrayY
+console.log(arrayMath.sum());
+
+// Computing for the summation of arrayX
+constructor (myArray) {
+this.myArray = myArray
+}
+
+getSum() {
+sum = 0;
+for (let i=0; i<this.myArray.length; i++) {
+sum += this.myArray[i];
+}
+return sum;
+}
+const arrayX = new MyClass([1, 2, 3, 4, 5])
+console.log(arrayX.getSum());
+
+// Computing for the summation of arrayY
+const arrayY = new MyClass([6, 7, 8, 9, 10]);
+console.log(arrayY.getSum());
+
+// Make summation of arrayX and arrayY to be properties of the class
+
+constructor (); {
+this.arrayMath.sum = [];
+}
+arrayMath.arrayMath.sum.push();
+console.log(arrayMath.arrayMath.sum);
+
+// Make a getter method that when called will return the value held by sum of arrayX and arrayY
+
+constructor (arrayX, arrayY); {
+this.array = arrayX;
+this.array = arrayY
+}
+getSum(); {
+result = [];
+
+
+for (let i=0, this.arrayX.length; i++){
+result[i] = this.arrayX[i] + this.arrayY[i];
+}
+return result;
+}
+arrayX = [1, 2, 3, 4, 5];
+arrayY = [6, 7, 8, 9, 10];
+const arraySum = new ArraySum(arrayX, arrayY);
+console.log(arraySum.sum);
+
+return {
+}
+arrayMath.sum();
+
+// Make product of arrayX and arrayY to be the properties of the class
+constructor (); {
+this.arrayMath.arrayMath.product = [];
+}
+arrayMath.arrayMath.product.push();
+console.log(arrayMath.arrayMath.product);
+
+// Make a getter method that when called will return the value held by the product of arrayX and arrayY
+getSum(); {
+result = [];
+}
+
+for (let i=0, this.arrayX.length; i++) {
+result[i] = this.arrayX * this.arrayY[i];
+}
+return result; {
+
+}
+arrayX = (1, 2, 3, 4, 5);
+arrayY = (6, 7, 8, 9, 10);
+const arrayProduct = new ArrayProduct(arrayX, arrayY);
+console.log(arrayProduct.product);
+
+return {
+}
+arrayMath.product();
+
+// Make a method that compute the sum of arrayX and arrayY of an arrow function
+const sumArrays = (arrayX, arrayY) => arrayX.map((1, 2, 3, 4, 5) + arrayY(6, 7, 8, 9, 10));
+console.log(sumArrays);
